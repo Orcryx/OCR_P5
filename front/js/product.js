9e7ff9bda71ac66573233f70e341d650d54de8b5
Orcryx <lauryannmagar@gmail.com>
2022-10-17 14:15:15 +0200

    Consol.log quantité et couleur de la sélection

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,6 @@
-var url =  window.location.href; // propriété qui permet de récupérer l'url d'un page courante 
+const url =  window.location.href; // propriété qui permet de récupérer l'url d'un page courante 
 console.log(' url du site est : ' + url);
-var newURL = new URL(url); // Créer un nouvel objet à partir de url
+const newURL = new URL(url); // Créer un nouvel objet à partir de url
 console.log(' newURL du site est : ' + newURL);
 const idProduct = newURL.searchParams.get("_id"); // Isolé l'ID qui est dans newURL avec .searcParams qui permet de récupérer le paramètre "_id" dans l'url avec .get
 console.log(' ID du produit est : ' +  idProduct);
@@ -20,12 +20,46 @@ function myProduct(product)
     document.querySelector("#title").innerHTML  +=`<h1 id="title">${product.name}</h1>`;
     document.querySelector("#price").innerHTML +=`<span id="price">${product.price}</span>`;
     document.querySelector("#description").innerHTML +=`<p id="description">${product.description}</p>`;
+    //boucle pour récupérer chaque couleur présente dans le tableau des couleurs
     for(let i=0; i < product.colors.length; i++ )
     {
         document.querySelector("#colors").innerHTML +=`<option value="${product.colors[i]}">${product.colors[i]}</option>`;
+    }  
+}
+
+/** *Retourner la valeur dans l'input id=quantity */
+function quantityProduct()
+{
+    return parseInt(document.getElementById("quantity").value);  
+}
+/** Sur evenement retourner valeur fonction quantityProduct() */
+
+const quantity = document.getElementById("quantity");
+quantity.addEventListener("change", ()=>
+    {
+        console.log('Dans changement ' + quantityProduct());
     }
-};
+)
 
 
-/** * Fonction pour modifier l'image */
-// function productImg (); 
+/** Sur evenement retourner valeur fonction colorProduct() */
+const color = document.getElementById("colors");
+color.addEventListener("change", ()=>
+    {
+        console.log(colorsProduct());
+    }
+)
+/** *Retourner la valeur dans option de id=colors */
+function colorsProduct()
+{
+    const colorSeclect = document.getElementById("colors").value;
+    return colorSeclect;
+}
+
+
+/** *Ajouter les données au localStorage lorsque évenement de click sur id=addToCart + modifie le comportement par défaut avant le submit pour permettre les verifications (voir plus bas)*/
+document.querySelector("#addToCart").addEventListener('click', (e) =>
+    { 
+    e.preventDefault();
+    }
+)
