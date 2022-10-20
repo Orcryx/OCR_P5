cfc0f26cb7f4b00a1017e2159f699ff790d98802
Orcryx <lauryannmagar@gmail.com>
2022-10-20 14:08:06 +0200

    Supprimer un élément dans le localStorage via le HTML et supprimer l'occurence dans le panier + reload page

--- front/js/cart.js
+++ front/js/cart.js
@@ -15,7 +15,7 @@ for(i=0; i<order.length ; i++)
                         <div class="cart__item__content__description">
                             <h2>${order[i].nameChooseProduct}</h2>
                             <p>${order[i].colorChooseProduct}</p>
-                            <p>${order[i].prixChooseProduct}€</p>
+                            <p>${order[i].prixChooseProduct * order[i].quantityChooseProduct} €</p>
                         </div>
                         <div class="cart__item__content__settings">
                             <div class="cart__item__content__settings__quantity">
@@ -23,9 +23,48 @@ for(i=0; i<order.length ; i++)
                                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${order[i].quantityChooseProduct}">
                             </div>
                             <div class="cart__item__content__settings__delete">
-                                <p class="deleteItem">Supprimer</p>
+                                <p class="deleteItem" canapeId="${order[i].idChooseProduct}" canapeColor="${order[i].colorChooseProduct}">Supprimer</p>
                             </div>
                         </div>
                     </div>
               </article>`;
     }
+   
+   
+/***Supprimer un élément dans le localStorage via le HTML et supprimer l'occurence dans le panier + reload page */
+    function deleteItem() {
+        let buttons = document.querySelectorAll('.deleteItem');
+        for (let button of Array.from(buttons)){
+            button.addEventListener("click", e => {
+                let canapeId = e.target.getAttribute("canapeId");
+                let canapeColor = e.target.getAttribute("canapeColor");
+                const searchDeleteItem = order.find(element => element.idChooseProduct == canapeId && element.colorChooseProduct == canapeColor);
+                order = order.filter(item => item != searchDeleteItem);
+                localStorage.setItem("sofa", JSON.stringify(order));
+                window.location.href = "cart.html";
+            })
+        }
+      }
+      
+      deleteItem();
+
+    // document.querySelector(".deleteItem").addEventListener('click', () =>{
+
+    //    let products = JSON.parse(localStorage.getItem('sofa'));
+    //    //console.log(products);
+    //    //let variable de controle id + couleur
+    //    let newproducts = products.filter(p);
+    //    localStorage.setItem('sofa', JSON.stringify(newproducts))
+    //    //appeler la fonction d'affichage des produits avec les nouvelles données
+       
+    // });
+
+// function modifProduct()
+// {
+//     let quantityOrder = document.querySelector('.itemQuantity');
+
+//     quantityOrder.addEventListener('change', () => 
+//     {
+        
+//     });
+// }
