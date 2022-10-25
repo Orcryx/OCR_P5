3e10fc19dc9b737d78d05c1ba9c2d5820d04a6f5
Orcryx <lauryannmagar@gmail.com>
2022-10-25 16:45:32 +0200

    Récupérer l'ID de l'url confirmation.html ET afficher le numéro de commande

---
+++ front/js/confirmation.js
@@ -0,0 +1,9 @@
+const url =  window.location.href; // propriété qui permet de récupérer l'url d'un page courante 
+//console.log(' url du site est : ' + url);
+const newURL = new URL(url); // Créer un nouvel objet à partir de url
+//console.log(' newURL du site est : ' + newURL);
+const idCommande = newURL.searchParams.get("_id"); // Isolé l'ID qui est dans newURL avec .searcParams qui permet de récupérer le paramètre "_id" dans l'url avec .get
+console.log(' ID du produit est : ' +  idCommande);
+
+document.querySelector('#orderId').innerHTML= idCommande;
+document.querySelector('#orderId').style.color = "#6599c2" ; 
