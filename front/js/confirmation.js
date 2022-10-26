13c5c178070ee361163a824f52659d9c23c0d0f8
Orcryx <lauryannmagar@gmail.com>
2022-10-26 14:38:15 +0200

    Comment console.log

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -3,7 +3,7 @@ const url =  window.location.href; // propriété qui permet de récupérer l'ur
 const newURL = new URL(url); // Créer un nouvel objet à partir de url
 //console.log(' newURL du site est : ' + newURL);
 const idCommande = newURL.searchParams.get("id"); // Isolé l'ID qui est dans newURL avec .searcParams qui permet de récupérer le paramètre "_id" dans l'url avec .get
-console.log(' ID du produit est : ' +  idCommande);
+//console.log(' ID du produit est : ' +  idCommande);
 
 document.querySelector('#orderId').innerHTML= idCommande;
 document.querySelector('#orderId').style.color = "#6599c2" ; 
