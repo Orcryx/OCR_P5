340d4fae746c8f34885b9ad8f2d863350f0da4c5
Orcryx <lauryannmagar@gmail.com>
2022-11-07 16:29:40 +0100

    Ajouter les calculs de prix et de quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -13,10 +13,35 @@ function getProduct ()
 }
 let Products = getProduct();
 
+
+
+/** Trouver les prix unitaire */
+function produitComplet()
+{
+    let Objet = [];
+    for(let i=0;i<Products.length;i++)
+    {
+        fetch(`http://localhost:3000/api/products/`)
+            .then(product => product.json())
+            .then(product =>
+                { 
+                    Objet = Products ;
+                    let data = product.find(element => element._id === Objet[i].idChooseProduct);
+                    Objet[i].price = data.price;
+                    //console.log(Objet);
+
+                    document.querySelector('#totalQuantity').innerHTML = calculNbArticle(Objet);  
+                    document.querySelector('#totalPrice').innerHTML= PrixTotal(Objet);
+                })
+            
+    }
+}
+produitComplet()
+
 /** Calculer le nombre de produit dans la commande */
 function calculNbArticle (Products)
 {
-//console.log(Products)
+console.log(Products)
 let nbArticles = 0;
     for(let i=0; i<Products.length; i++)
     {
@@ -26,49 +51,16 @@ return nbArticles ;
 }
 //console.log('Il y a tant de produits dans la commande : ' + calculNbArticle (Products))
 
-/** Trouver les prix unitaire */
-function produitComplet()
-{
-    let Objet = [];
-    for(let i=0;i<Products.length;i++)
-    {
-
-        fetch(`http://localhost:3000/api/products/`)
-            .then(product => product.json())
-            .then(product =>
-                { 
-                    Objet = Products ;
-                    //console.log(Objet);
-                    let data = product.find(element => element._id === Objet[i].idChooseProduct);
-                    //console.log(data);
-
-                    if(Objet[i].idChooseProduct = data._id);
-                    { 
-                        //console.log("mon prix : " + data.price);
-                        Objet[i].price = data.price;
-                        console.log(Objet)
-                        //return Objet;
-                    }
-                
-                })
-
-    }
-}
-produitComplet()
-
-// let myProduct = produitComplet()
-// console.log(myProduct)
 
 /** Calculer le prix total dans la commande */
-//  function PrixTotal (prices)
-//  {
-//     console.log(prices);
-//      let prix = 0;
-//      for(let i=0; i<prices.length; i++)
-//      {
-//          prix += prices[i];
-//      }
-//  return prix;
-//  }
-//  console.log(PrixTotal(prices));
+ 
+ function PrixTotal (products)
+ {
+     let prix = 0;
+     for(let i=0; i<products.length; i++)
+     {
+         prix += products[i].quantityChooseProduct * products[i].price ;
+     }
+ return prix;
+ }
  
