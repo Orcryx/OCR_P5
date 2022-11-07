1709b3baa4d677d406b89e1f556c05b2397535f5
Orcryx <lauryannmagar@gmail.com>
2022-11-07 16:45:25 +0100

    Ajouter Delete sans new affichage

--- front/js/cart.js
+++ front/js/cart.js
@@ -15,8 +15,8 @@ let Products = getProduct();
 
 
 
-/** Trouver les prix unitaire */
-function produitComplet()
+/** Afficher le panier*/
+function Basket()
 {
     let Objet = [];
     for(let i=0;i<Products.length;i++)
@@ -29,19 +29,40 @@ function produitComplet()
                     let data = product.find(element => element._id === Objet[i].idChooseProduct);
                     Objet[i].price = data.price;
                     //console.log(Objet);
-
+                    document.querySelector("#cart__items").innerHTML +=`
+                                          <article class="cart__item" data-id="${Objet[i].idChooseProduct}" data-color="${Objet[i].colorChooseProduct}">
+                                              <div class="cart__item__img">
+                                                  <img src="${data.imageUrl}" alt="${data.altTxt}">
+                                              </div>
+                                              <div class="cart__item__content">
+                                                  <div class="cart__item__content__description">
+                                                      <h2>${Objet[i].idChooseProduc}</h2>
+                                                      <p>${Objet[i].colorChooseProduct}</p>
+                                                      <p>${Objet[i].price} €</p>
+                                                  </div>
+                                                  <div class="cart__item__content__settings">
+                                                      <div class="cart__item__content__settings__quantity">
+                                                          <p>Qté :</p>
+                                                          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${Objet[i].quantityChooseProduct}">
+                                                      </div>
+                                                      <div class="cart__item__content__settings__delete">
+                                                          <p class="deleteItem">Supprimer</p>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                       </article>`;
                     document.querySelector('#totalQuantity').innerHTML = calculNbArticle(Objet);  
                     document.querySelector('#totalPrice').innerHTML= PrixTotal(Objet);
+                    deleteItem(Objet)
                 })
-            
     }
 }
-produitComplet()
+Basket()
 
 /** Calculer le nombre de produit dans la commande */
 function calculNbArticle (Products)
 {
-console.log(Products)
+//console.log(Products)
 let nbArticles = 0;
     for(let i=0; i<Products.length; i++)
     {
@@ -64,3 +85,27 @@ return nbArticles ;
  return prix;
  }
  
+  /** Supprimer un élément du panier */
+ function deleteItem(products){
+     let buttons = document.querySelectorAll('.deleteItem');
+     for (let button of Array.from(buttons))
+     {
+         //console.log(buttons)
+         button.addEventListener("click", (e) => {
+                 let idKanap  = e.target.closest('article').getAttribute("data-id");
+                 console.log(idKanap);
+                 let colorKanap = e.target.closest('article').getAttribute("data-color"); 
+                 console.log(colorKanap); 
+                 // créer un tableau filtréer qui cherche en fonction d'élément présent dans order
+                 let key = products.find(element => element.idChooseProduct === idKanap && element.colorChooseProduct == colorKanap);
+                 console.log(key);
+                 // filtrer l'item qui est égale à ma clé dans le localstorage et remplacer le tableau order par le tableau order filtré
+                 let order = products.filter(item => item != key);
+                 //écrir dans le localStorage : "sofa" prend la valeur de l'objet order devenu une chaine
+                 localStorage.setItem("sofa", JSON.stringify(order));
+                 // //Recharger la page pour actualiser l'affichage : voir pour trouver meilleure solution 
+                 Basket();
+             });
+     }
+       }
+       deleteItem();
